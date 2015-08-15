0e3526d98accd6308f598252404b79f6c3390e34
Patrick Golden <ptgolden0@gmail.com>
2015-08-15 15:24:48 -0400

    Initial project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var React = require('react')
+  , Application = require('./views/application.jsx')
+
+React.render(<Application />, document.body);
---
+++ src/views/application.jsx
@@ -0,0 +1,14 @@
+"use strict";
+
+var React = require('react')
+
+module.exports = React.createClass({
+  displayName: 'Application',
+  render: function () {
+    return (
+      <main className="m3">
+        <h1>Application</h1>
+      </main>
+    )
+  }
+});
