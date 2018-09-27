ca4786904ef857e5c5f4a143ec1796e9198a2d52
Patrick Golden <ptgolden0@gmail.com>
2018-09-27 10:32:49 -0400

    Update heat map: Outline hovered treatment and show its label

--- src/components/Table/HeatMap.js
+++ src/components/Table/HeatMap.js
@@ -3,6 +3,7 @@
 const h = require('react-hyperscript')
     , R = require('ramda')
     , d3 = require('d3')
+    , React = require('react')
     , styled = require('styled-components').default
     , { connect } = require('react-redux')
 
@@ -13,70 +14,100 @@ const HeatMapContainer = styled.div`
 `
 
 
-function HeatMap ({
-  grid,
-  gene,
-  rpkmsForTreatmentGene,
-  treatments,
-}) {
-  if (!grid || !gene || !rpkmsForTreatmentGene) {
-    return null
+class HeatMap extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      hoveredTreatment: null,
+    }
   }
 
-  const rpkms = grid.map(row =>
-    row.map(treatment =>
-      treatment && d3.mean(rpkmsForTreatmentGene(treatment, gene))))
+  render() {
+    const { grid, gene, rpkmsForTreatmentGene, treatments } = this.props
+        , { hoveredTreatment } = this.state
 
-  const maxRPKM = R.reduce(R.max, 1, R.flatten(rpkms).filter(R.identity))
+    if (!grid || !gene || !rpkmsForTreatmentGene) {
+      return null
+    }
 
-  const colorScale = d3.scaleSequential(d3.interpolateReds)
-    .domain([0, maxRPKM])
+    const rpkms = grid.map(row =>
+      row.map(treatment =>
+        treatment && d3.mean(rpkmsForTreatmentGene(treatment, gene))))
 
-  const xScale = d3.scaleLinear()
-    .domain([0, grid[0].length])
-    .range([0, 100])
+    const maxRPKM = R.reduce(R.max, 1, R.flatten(rpkms).filter(R.identity))
 
-  const yScale = d3.scaleLinear()
-    .domain([0, grid.length])
-    .range([0, 100])
+    const colorScale = d3.scaleSequential(d3.interpolateOranges)
+      .domain([0, maxRPKM])
 
-  const squares = R.pipe(
-    grid => grid.map((row, i) =>
-      row.map((treatment, j) => treatment && {
-        treatment,
-        style: {
-          background: colorScale(rpkms[i][j]),
-          left: `${xScale(j)}%`,
-          width: `calc(${xScale(1)}% - 1px)`,
-          top: `${yScale(i)}%`,
-          height: `${yScale(1)}%`,
-        },
-      })
-    ),
-    R.chain(R.filter(R.identity))
-  )(grid)
+    const xScale = d3.scaleLinear()
+      .domain([0, grid[0].length])
+      .range([0, 100])
 
-  return (
-    h('div', {
-      style: {
-        padding: '1em 0',
-        display: 'flex',
-        flexDirection: 'column',
-      },
-    }, [
-      h('h2', gene),
-      h(HeatMapContainer, squares.map(square =>
-        h('div', {
-          title: treatments[square.treatment].label,
-          style: Object.assign({
-            position: 'absolute',
-            border: '1px solid #333',
-            boxSizing: 'content-box',
-          }, square.style),
+    const yScale = d3.scaleLinear()
+      .domain([0, grid.length])
+      .range([0, 100])
+
+    const squares = R.pipe(
+      grid => grid.map((row, i) =>
+        row.map((treatment, j) => treatment && {
+          treatment,
+          style: {
+            background: colorScale(rpkms[i][j]),
+            left: `${xScale(j)}%`,
+            width: `calc(${xScale(1)}% - 1px)`,
+            top: `${yScale(i)}%`,
+            height: `${yScale(1)}%`,
+          },
         })
-      )),
-    ])
-  )
+      ),
+      R.chain(R.filter(R.identity))
+    )(grid)
+
+    return (
+      h('div', {
+        style: {
+          fontFamily: 'SourceSansPro',
+          fontSize: 18,
+          padding: '1em 0',
+          display: 'flex',
+          flexDirection: 'column',
+        },
+      }, [
+        h('div', {
+          style: {
+            display: 'flex',
+            justifyContent: 'space-between',
+          }
+        }, [
+          h('h2', gene),
+
+          hoveredTreatment && treatments[hoveredTreatment].label,
+        ]),
+        h(HeatMapContainer, squares.map(square =>
+          h('div', {
+            title: treatments[square.treatment].label,
+            onMouseEnter: () => {
+              this.setState({ hoveredTreatment: square.treatment })
+            },
+            onMouseLeave: () => {
+              this.setState({ hoveredTreatment: null })
+            },
+            style: Object.assign({
+              position: 'absolute',
+              boxSizing: 'content-box',
+              marginLeft: square.treatment === hoveredTreatment ? '-2px' : 0,
+              marginTop: square.treatment === hoveredTreatment ? '-2px' : 0,
+              zIndex: square.treatment === hoveredTreatment ? 1 : 'unset',
+              border: square.treatment === hoveredTreatment
+                ? '4px solid #333'
+                : '1px solid #333'
+            }, square.style),
+          })
+        )),
+      ])
+    )
+  }
 }
 
 
