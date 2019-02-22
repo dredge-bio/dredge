97663a235c5c88421722b8c8db4331201301e60b
Patrick Golden <ptgolden0@gmail.com>
2019-02-22 16:08:06 -0500

    Add abundance limits to project editing form + tidy up styling

--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -41,13 +41,25 @@ const Field = styled(Box)`
 .label-text {
   display: inline-block;
   font-weight: bold;
+  font-size: 18px;
   margin-bottom: 4px;
+}
+
+.axis-label-text {
+  font-weight: bold;
+}
+
+.axis-label-type {
+  margin-right: 2px;
 }
 
 input {
   padding: 8px;
   border-radius: 4px;
   border: 1px solid #ccc;
+}
+
+input[type="text"] {
   width: 384px;
 }
 
@@ -79,6 +91,20 @@ function resolveURL(base, path) {
   }
 }
 
+const LimitInputContainer = styled(Box)`
+  width: 64px;
+  margin-right: 8px;
+`
+
+function LimitInput({ value, onChange }) {
+  return h(LimitInputContainer, {
+    as: 'input',
+    type: 'number',
+    value,
+    onChange,
+  })
+}
+
 function Input(props) {
   const { showURL, isRelativeURL } = props
 
@@ -95,11 +121,16 @@ function Input(props) {
 
   return (
     h(Field, { mb: 4, ['data-required']: !!props.required }, [
-      h('label', {}, [].concat(
+      h('label', [
         h('span.label-text', props.label),
         h('br'),
-        h('input', Object.assign({ type: 'text' }, innerProps))
-      )),
+        h('input', Object.assign({
+          type: 'text',
+          autoCorrect: 'off',
+          autoCapitalize: 'off',
+          spellCheck: false,
+        }, innerProps))
+      ]),
 
       !props.showURL ? null : (
         h(Box, { className: 'resolved-url', my: 2 }, [
@@ -160,9 +191,10 @@ module.exports = Navigable(class NewProject extends React.Component {
     this.debouncedPersist = debounce(this.persist, 250)
   }
 
-  setField(fieldName, fn=R.identity) {
+  setField(fieldName) {
+    const lens = R.lensPath([].concat(fieldName))
     return e => {
-      this.setState({ [fieldName]: fn(e.target.value) }, this.debouncedPersist)
+      this.setState(R.set(lens, e.target.value), this.debouncedPersist)
     }
   }
 
@@ -219,84 +251,127 @@ module.exports = Navigable(class NewProject extends React.Component {
         ]),
 
         h(Box, [
-          h(Heading, { as: 'h2', mb: 2 }, 'Basics'),
+          h(Heading, { as: 'h2', mb: 2 }, 'Configuration'),
 
-          h(Box, [
-            h(Input, {
-              label: 'Project name',
-              required: true,
-              onChange: this.setField('label'),
-              value: this.state.label,
-            }),
+          h(Input, {
+            label: 'Project name',
+            required: true,
+            onChange: this.setField('label'),
+            value: this.state.label,
+          }),
 
-            h(Input, {
-              label: 'Root URL',
-              help: 'The URL that will be the basis for all further URLs. If this value is not an absolute URL, it will be resolved relative to the page on which DrEdGE is hosted.',
-              required: true,
-              onChange: this.setField('baseURL'),
-              value: this.state.baseURL,
-              showURL: resolve(''),
-              isRelativeURL,
-            }),
+          h(Input, {
+            label: 'Root URL',
+            help: h('div', [
+              h(Box, { as: 'p', mb: 2 }, 'The URL that will be the basis for all further URLs. If this value is not an absolute URL, it will be resolved relative to the page on which DrEdGE is hosted.'),
+              h('p', 'When you save this configuration file, it must be placed in this directory.'),
+            ]),
+            required: true,
+            onChange: this.setField('baseURL'),
+            value: this.state.baseURL,
+            showURL: resolve(''),
+            isRelativeURL,
+          }),
 
-            h(Input, {
-              label: 'Treatments URL',
-              help: 'The URL that contains information about individual treatments in the dataset',
-              required: true,
-              onChange: this.setField('treatments'),
-              value: this.state.treatments,
-              showURL: this.state.treatments && resolve(this.state.treatments),
-              isRelativeURL,
-            }),
+          h(Input, {
+            label: 'Treatments URL',
+            help: 'The URL that contains information about individual treatments in the dataset',
+            required: true,
+            onChange: this.setField('treatments'),
+            value: this.state.treatments,
+            showURL: this.state.treatments && resolve(this.state.treatments),
+            isRelativeURL,
+          }),
+
+          h(Input, {
+            label: 'Pairwise comparison URL template',
+            help: h(Box, [
+              h(Box, { as: 'p', mb: 2 }, 'A template to generate the URLs to pairwise comparison data. The characters \'%A\' will be replaced with the name of the first treatment, and \'%B\' the second.'),
+              h('p', [
+                'For example: If my dataset contained the pairwise comparison for treatments T1 and T2 in the directory ',
+                h('code', 'pairwise_tests/T1-T2.csv'),
+                ' the value in this field would be ',
+                h('code', 'pairwise_tests/%A-%B.csv'),
+              ]),
+            ]),
+            onChange: this.setField('pairwiseName'),
+            value: this.state.pairwiseName,
+            showURL: this.state.pairwiseName && resolve(this.state.pairwiseName),
+            isRelativeURL,
+          }),
+
+          h(Field, { mb: 4 }, [
+            h('label', [
+              h('span.label-text', 'MA plot limits'),
+            ]),
+
+            h(Box, [
+              h('span.axis-label-text', 'X axis'),
+              ' (log₂ Average Transcript Abundance)',
+              h(Flex, { alignItems: 'center', mt: 1, mb: 2 }, [
+                h('span.axis-label-type', 'min'),
+                h(LimitInput, {
+                  value: this.state.abundanceLimits[0][0],
+                  onChange: this.setField(['abundanceLimits', 0, 0]),
+                }),
+
+                h('span.axis-label-type', 'max'),
+                h(LimitInput, {
+                  value: this.state.abundanceLimits[0][1],
+                  onChange: this.setField(['abundanceLimits', 0, 1]),
+                }),
+              ]),
+            ]),
+
+            h(Box, [
+              h('span.axis-label-text', 'Y axis'),
+              ' (log₂ Fold Change)',
+              h(Flex, { alignItems: 'center', mt: 1, mb: 2 }, [
+                h('span.axis-label-type', 'min'),
+                h(LimitInput, {
+                  value: this.state.abundanceLimits[1][0],
+                  onChange: this.setField(['abundanceLimits', 1, 0]),
+                }),
+
+                h('span.axis-label-type', 'max'),
+                h(LimitInput, {
+                  value: this.state.abundanceLimits[1][1],
+                  onChange: this.setField(['abundanceLimits', 1, 1]),
+                }),
+              ]),
+            ]),
+
+            h(Box, { mt: 1, className: 'help-text' }, [
+              h('p', 'The limits for the X and Y axes on the MA Plot. Set these to values such that all of the data from your pairwise comparisons will fall within the plot.'),
+            ]),
           ]),
 
-          h(Heading, { as: 'h2', mb: 2, mt: 4 }, 'Configuration'),
+          h(Input, {
+            label: 'Transcript aliases URL',
+            help: 'The URL that contains information about alternate names for transcripts in the dataset. This should be a CSV file whose first column contains the canonical label for a transcript, and whose other columns contain alternate names.',
+            onChange: this.setField('transcriptAliases'),
+            value: this.state.transcriptAliases,
+            showURL: this.state.transcriptAliases && resolve(this.state.transcriptAliases),
+            isRelativeURL,
+          }),
 
-          h(Box, [
-            h(Input, {
-              label: 'Pairwise comparison URL template',
-              help: h(Box, [
-                h(Box, { as: 'p', mb: 2 }, 'A template to generate the URLs to pairwise comparison data. The characters \'%A\' will be replaced with the name of the first treatment, and \'%B\' the second.'),
-                h('p', [
-                  'For example: If my dataset contained the pairwise comparison for treatments T1 and T2 in the directory ',
-                  h('code', 'pairwise_tests/T1-T2.csv'),
-                  ' the value in this field would be ',
-                  h('code', 'pairwise_tests/%A-%B.csv'),
-                ]),
-              ]),
-              onChange: this.setField('pairwiseName'),
-              value: this.state.pairwiseName,
-              showURL: this.state.pairwiseName && resolve(this.state.pairwiseName),
-              isRelativeURL,
-            }),
+          h(Input, {
+            label: 'Abundance measures URL',
+            help: 'The URL of the file containing abundance measures for each transcript by treatement.',
+            onChange: this.setField('abundanceMeasures'),
+            value: this.state.abundanceMeasures,
+            showURL: this.state.abundanceMeasures && resolve(this.state.abundanceMeasures),
+            isRelativeURL,
+          }),
 
-            h(Input, {
-              label: 'Transcript aliases URL',
-              help: 'The URL that contains information about alternate names for transcripts in the dataset. This should be a CSV file whose first column contains the canonical label for a transcript, and whose other columns contain alternate names.',
-              onChange: this.setField('transcriptAliases'),
-              value: this.state.transcriptAliases,
-              showURL: this.state.transcriptAliases && resolve(this.state.transcriptAliases),
-              isRelativeURL,
-            }),
-
-            h(Input, {
-              label: 'Abundance measures URL',
-              help: 'The URL of the file containing abundance measures for each transcript by treatement.',
-              onChange: this.setField('abundanceMeasures'),
-              value: this.state.abundanceMeasures,
-              showURL: this.state.abundanceMeasures && resolve(this.state.abundanceMeasures),
-              isRelativeURL,
-            }),
-
-            h(Input, {
-              label: 'Diagram URL',
-              help: 'The URL of the SVG diagram for this project',
-              onChange: this.setField('diagram'),
-              value: this.state.diagram,
-              showURL: this.state.diagram && resolve(this.state.diagram),
-              isRelativeURL,
-            }),
-          ])
+          h(Input, {
+            label: 'Diagram URL',
+            help: 'The URL of the SVG diagram for this project',
+            onChange: this.setField('diagram'),
+            value: this.state.diagram,
+            showURL: this.state.diagram && resolve(this.state.diagram),
+            isRelativeURL,
+          }),
         ]),
       ])
     )
