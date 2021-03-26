edbfc0558d365bd5d599f9de5c9226b975aaff6b
Patrick Golden <ptgolden0@gmail.com>
2021-03-26 16:37:52 -0400

    Add fields for documentation, transcript aliases, diagram...

--- src/projects/fields.ts
+++ src/projects/fields.ts
@@ -8,16 +8,18 @@ import * as types from '../ts_types'
 
 import { PathReporter } from 'io-ts/PathReporter'
 
+import MarkdownIt from 'markdown-it'
+
 async function asyncIdentity<T>(x: T) { return x }
 
 
-class TreatmentField<T, V=T> {
+class ProjectField<T, V=T> {
   label: string;
   required: boolean;
   cached: boolean;
   decoder: t.Decoder<unknown, T>;
   processResponse: (resp: Response) => Promise<any>;
-  processValidated: (validated: T) => Promise<V>;
+  processValidated: (validated: T, treatments?: Record<string, types.ProjectTreatment>) => Promise<V>;
 
   constructor(config: {
     label: string,
@@ -25,7 +27,7 @@ class TreatmentField<T, V=T> {
     cached: boolean,
     decoder: t.Decoder<unknown, T>,
     processResponse?: (resp: Response) => Promise<any>,
-    processValidated: (validated: T) => Promise<V>;
+    processValidated: (validated: T, treatments?: Record<string, types.ProjectTreatment>) => Promise<V>;
   }) {
     this.label = config.label;
     this.required = config.required;
@@ -48,7 +50,8 @@ class TreatmentField<T, V=T> {
 
   async validateFromURL(
     url: string,
-    makeLog: (label: string, url: string) => (status: types.LogStatus, message?: string) => void
+    makeLog: (label: string, url: string) => (status: types.LogStatus, message?: string) => void,
+    treatments?: Record<string, types.ProjectTreatment>
   ) {
     const fullURL = new URL(url, window.location.toString()).href
         , log = makeLog(this.label, fullURL)
@@ -70,7 +73,7 @@ class TreatmentField<T, V=T> {
             throw Error(errString.join('\n'))
           },
           async val => {
-            const processed = await this.processValidated(val)
+            const processed = await this.processValidated(val, treatments)
 
             await log('OK')
 
@@ -99,7 +102,7 @@ class TreatmentField<T, V=T> {
   },
   */
 
-export const treatments = new TreatmentField<
+export const treatments = new ProjectField<
   Record<string, types.ProjectTreatment>
 >({
   label: 'Project treatments',
@@ -114,7 +117,7 @@ export const treatments = new TreatmentField<
   )
 })
 
-export const abundanceMeasures = new TreatmentField<
+export const abundanceMeasures = new ProjectField<
   [ replicateRow: string, abundances: Array<string> ],
   {
     transcripts: Array<string>,
@@ -161,7 +164,7 @@ export const abundanceMeasures = new TreatmentField<
   processResponse(resp) {
     return resp.text()
   },
-  decoder: withValidate( t.tuple([
+  decoder: withValidate(t.tuple([
     t.string,
     t.array(t.string),
   ]), (input, context) => {
@@ -179,4 +182,156 @@ export const abundanceMeasures = new TreatmentField<
 
     return t.success([ replicateRow, abundanceRows] )
   })
+})
+
+function trim(x: string) {
+  return x.trim()
+}
+
+function notBlank(x: string) {
+  return x
+}
+
+export const aliases = new ProjectField<
+  string,
+  Record<string, Array<string>>
+>({
+  label: 'Transcript aliases',
+  required: false,
+  cached: false,
+  processResponse(resp) {
+    return resp.text()
+  },
+  decoder: t.string,
+  async processValidated(str) {
+    const aliases: Record<string, Array<string>> = {}
+
+    let i = 0
+
+    for (const line of str.split('\n')) {
+      const [ canonical, ...others ] = line.split(/\t|,/).map(trim).filter(notBlank)
+      aliases[canonical] = others
+      i++
+      if (i % 1500 === 0) await delay(0)
+    }
+
+    return aliases
+  }
+})
+
+export const readme = new ProjectField<
+  string
+>({
+  label: 'Project documentation',
+  required: false,
+  cached: false,
+  processResponse(resp) {
+    return resp.text()
+  },
+  decoder: t.string,
+  async processValidated(str) {
+    const md = new MarkdownIt()
+    return md.render(str)
+  }
+})
+
+function cleanSVGString(svgString: string, treatments: Record<string, types.ProjectTreatment>) {
+  const parser = new DOMParser()
+      , svgDoc = parser.parseFromString(svgString, 'image/svg+xml')
+      , iterator = svgDoc.createNodeIterator(svgDoc, NodeFilter.SHOW_ELEMENT)
+      , treatmentNames = new Set(Object.keys(treatments))
+
+  let curNode: Node | null
+
+  svgDoc.createNodeIterator
+
+  Array.from(svgDoc.querySelectorAll('title')).forEach(el => {
+    el.parentNode?.removeChild(el)
+  })
+
+  const anchorsToRemove = []
+
+  while ( (curNode = iterator.nextNode()) ) {
+    const node = curNode as SVGElement
+
+    switch (node.nodeName.toLowerCase()) {
+      case 'path':
+      case 'rect':
+      case 'circle':
+      case 'elipse':
+      case 'polyline':
+      case 'polygon': {
+        let treatment = null
+
+        const popTreatmentFromAttr = (attr: string) => {
+          treatment = node.getAttribute(attr)
+
+          if (treatment !== null && treatmentNames.has(treatment)) {
+            node.removeAttribute(attr)
+            return true
+          }
+          return false
+        }
+
+        popTreatmentFromAttr('id') || popTreatmentFromAttr('name')
+
+        if (treatment) {
+          const { label } = treatments[treatment]
+
+          node.setAttribute('data-treatment', treatment)
+
+          const titleEl = document.createElement('title')
+          titleEl.textContent = label || treatment
+
+          node.appendChild(titleEl)
+          treatmentNames.delete(treatment)
+
+          // Illustrator, for some reason, makes all paths the child of
+          // an anchor tag. That messes up our clicking business. We
+          // could probably preventDefault() or stopPropagation()
+          // somewhere, but I'm just removing them here.
+          const replaceParent = (
+            node.parentNode?.nodeName.toLowerCase() === 'a' &&
+            node.parentNode?.children.length === 1
+          )
+
+          if (replaceParent) {
+            anchorsToRemove.push(node.parentNode)
+          }
+        }
+
+        break;
+      }
+    }
+
+    // Remove ID, since multiple instances of this SVG will be in the
+    // document. Alternatively, the whole thing could always be wrapped
+    // in an iframe, but that would require inter-frame communication,
+    // which seems like a pain in the ass.
+    node.removeAttribute('id')
+  }
+
+  anchorsToRemove.forEach(el => {
+    if (!el) return
+    (el as SVGElement).replaceWith(el.children[0])
+  })
+
+  return svgDoc.documentElement.outerHTML
+}
+
+export const svg = new ProjectField<
+  string
+>({
+  label: 'SVG diagram',
+  required: false,
+  cached: false,
+  processResponse(resp) {
+    return resp.text()
+  },
+  decoder: t.string,
+  async processValidated(str, treatments) {
+    if (!treatments) throw new Error('Can\'t parse SVG without treatments')
+
+    return cleanSVGString(str, treatments)
+  }
 })
