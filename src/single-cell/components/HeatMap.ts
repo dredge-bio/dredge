7d0540dfc22b9c9f12252884e00735c694fb41ad
Patrick Golden <ptgolden0@gmail.com>
2021-07-23 12:50:04 -0400

    Clean up HeatMap function, remove function drawing it with lines

--- src/single-cell/components/HeatMap.ts
+++ src/single-cell/components/HeatMap.ts
@@ -35,106 +35,12 @@ const showClusters = [
   '20',
 ]
 
-function drawHeatmapWithLines(
-  canvasEl: HTMLCanvasElement,
-  clusters: SeuratClusterMap,
-  scDataset: ReturnType<typeof useSeuratDataset>,
-  rect: { width: number, height: number }
-) {
-    let cellCount: number = 0
-
-    const zScoresByTranscript = new Map(transcripts.map(transcript => {
-      const zScoresForTranscript = scDataset.getScaledCountsForTranscript(transcript)
-
-      const clusterMap: Map<string, number[]> = new Map(
-        showClusters.map(cluster => [ cluster, [] ]))
-
-      zScoresForTranscript.forEach((zScore, cell) => {
-        if (!clusterMap.has(cell.clusterID)) return
-        clusterMap.get(cell.clusterID)!.push(zScore)
-        cellCount += 1
-      })
-
-      return [ transcript, clusterMap ]
-    }))
-
-    cellCount = cellCount / transcripts.length
-
-    const ctx = canvasEl.getContext('2d')!
-
-    const colorScale = d3.scaleLinear<string>()
-      .domain([ -.75, 1, 2.5 ])
-      .range([ '#ff00ff', 'black', 'yellow' ])
-      .clamp(true)
-
-    const w = (rect.width - 32) / cellCount
-        , h = 200 / transcripts.length
-
-    // ctx.scale(.1, .1)
-    let prevClusterStart = 16
-      , clustersDrawn = false
-
-
-    Array.from(zScoresByTranscript).forEach(([ transcript, zScoresByCluster ], transcriptIdx) => {
-      let idx = 0
-
-      if (transcriptIdx > 0) {
-        clustersDrawn = true
-      }
-
-      Array.from(zScoresByCluster).forEach(([ clusterID, zScoresByCell ], clusterIdx) => {
-        if (!clustersDrawn) {
-          const cluster = clusters.get(clusterID)!
-              , clusterStart = prevClusterStart
-              , clusterWidth = zScoresByCell.length * w
-
-          ctx.fillStyle = cluster.color
-          ctx.fillRect(clusterStart, 80, clusterWidth, 16)
-          prevClusterStart += clusterWidth
-
-          ctx.fillStyle = 'black'
-          ctx.textAlign = 'center'
-          ctx.font = '24px sans-serif'
-          ctx.fillText(cluster.label, clusterStart + clusterWidth / 2, 64, clusterWidth)
-        }
-
-        Array.from(zScoresByCell).forEach((zScore, cellIdx) => {
-          ctx.fillStyle = colorScale(zScore)
-          ctx.fillRect(16 + idx * w, 100 + transcriptIdx * h, w, h)
-          /*
-          debugger;
-          if (zScore === curZScore) {
-            consecZScores += 1
-          } else {
-            if (curZScore !== null) {
-              ctx.fillStyle = colorScale(curZScore)
-              ctx.fillRect(curZScoreStart * w, transcriptIdx * h, w * consecZScores, h)
-            }
-
-            curZScore = zScore
-            curZScoreStart = idx
-            consecZScores = 1
-          }
-          */
-          idx += 1
-        })
-      })
-    })
-
-    colorScale.ticks(1000).forEach((num, i) => {
-      ctx.fillStyle = colorScale(num)
-      ctx.fillRect(16 + i * .3235, 100 + (transcripts.length + 2) * h, .3235, h)
-    })
-}
-
 function drawHeatmapWithBlocks(
   canvasEl: HTMLCanvasElement,
   clusters: SeuratClusterMap,
   scDataset: ReturnType<typeof useSeuratDataset>,
   rect: { width: number, height: number }
 ) {
-    let cellCount: number = 0
-
     const zScoresByTranscript = new Map(transcripts.map(transcript => {
       const zScoresForTranscript = scDataset.getScaledCountsForTranscript(transcript)
 
@@ -144,15 +50,10 @@ function drawHeatmapWithBlocks(
       zScoresForTranscript.forEach((zScore, cell) => {
         if (!clusterMap.has(cell.clusterID)) return
         clusterMap.get(cell.clusterID)!.push(zScore)
-        cellCount += 1
       })
 
       return [ transcript, clusterMap ]
     }))
-
-    cellCount = cellCount / transcripts.length
-
-    const { width, height } = rect
 
     const ctx = canvasEl.getContext('2d')!
 
@@ -173,8 +74,6 @@ function drawHeatmapWithBlocks(
       , clustersDrawn = false
 
     Array.from(zScoresByTranscript).forEach(([ transcript, zScoresByCluster ], transcriptIdx) => {
-      let idx = 0
-
       if (transcriptIdx > 0) {
         clustersDrawn = true
       }
@@ -205,27 +104,6 @@ function drawHeatmapWithBlocks(
         const meanZScore = d3.mean(zScoresByCell)!
         ctx.fillStyle = colorScale(meanZScore)
         ctx.fillRect(PADDING_LEFT + clusterIdx * w, 100 + transcriptIdx * h, w, h)
-
-        Array.from(zScoresByCell).forEach((zScore, cellIdx) => {
-          // ctx.fillStyle = colorScale(zScore)
-          // ctx.fillRect(16 + idx * w, 100 + transcriptIdx * h, w, h)
-          /*
-          debugger;
-          if (zScore === curZScore) {
-            consecZScores += 1
-          } else {
-            if (curZScore !== null) {
-              ctx.fillStyle = colorScale(curZScore)
-              ctx.fillRect(curZScoreStart * w, transcriptIdx * h, w * consecZScores, h)
-            }
-
-            curZScore = zScore
-            curZScoreStart = idx
-            consecZScores = 1
-          }
-          */
-          idx += 1
-        })
       })
     })
 
