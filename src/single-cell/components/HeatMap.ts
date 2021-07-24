91fd4476b55dc8f68a20d3ac13e0ba4596ebdafe
Patrick Golden <ptgolden0@gmail.com>
2021-07-24 14:16:11 -0400

    Make drawing the heatmap more typed and logical
    Also don't hardcode grid height

--- src/single-cell/components/HeatMap.ts
+++ src/single-cell/components/HeatMap.ts
@@ -3,7 +3,7 @@ import * as d3 from 'd3'
 import { useEffect, useRef } from 'react'
 import { useSized } from '@dredge/main'
 import { useView, useSeuratDataset } from '../hooks'
-import { SeuratClusterMap } from '../types'
+import { SeuratCluster } from '../types'
 
 const transcripts = [
   'ttc36',
@@ -19,131 +19,249 @@ const PADDING_LEFT = 16
     , GRID_GAP = 3
     , CLUSTER_LEGEND_GAP = 10
     , TEXT_GAP = 6
-    , TRANSCRIPT_LABEL_WIDTH = 144
-    , CLUSTERS_START_TOP = PADDING_TOP + CLUSTER_LEGEND_HEIGHT
-    , CLUSTERS_START_LEFT = PADDING_LEFT + TRANSCRIPT_LABEL_WIDTH + TEXT_GAP
+    , TRANSCRIPT_LABEL_WIDTH = 120
     , LEGEND_PADDING_TOP = 20
-    , LEGEND_TICKS = 1000
-    , LEGEND_WIDTH = 300
-    , CLUSTER_GRID_HEIGHT = 100
+    , SCALE_HEIGHT = 56
+    , SCALE_LEGEND_HEIGHT = 10
+    , SCALE_TICKS = 1000
+    , SCALE_WIDTH = 300
 
-function drawHeatmapWithBlocks(
-  canvasEl: HTMLCanvasElement,
-  clusterMap: SeuratClusterMap,
-  scDataset: ReturnType<typeof useSeuratDataset>,
-  rect: { width: number, height: number }
+type CanvasText = {
+  text: string;
+  font: string;
+  color: string;
+  align: CanvasTextAlign;
+  baseline: CanvasTextBaseline;
+  x: number;
+  y: number;
+  maxWidth?: number;
+}
+
+type CanvasRect = {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  color: string;
+}
+
+type HeatMapSquare = CanvasRect & {
+  transcript: string;
+}
+
+type HeatMapCluster = {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  squares: HeatMapSquare[];
+  legend: {
+    bar: CanvasRect;
+    label: CanvasText;
+  };
+}
+
+type HeatMapData = {
+  clusters: HeatMapCluster[];
+  transcriptLabels: CanvasText[];
+  scale: {
+    marks: CanvasRect[];
+    labels: CanvasText[];
+  };
+}
+
+type ZScoreByCluster = Map<string, {
+  cluster: SeuratCluster;
+  transcripts: Map<string, number[]>;
+}>
+
+function drawRect(
+  ctx: CanvasRenderingContext2D,
+  rect: CanvasRect
 ) {
-  const clusters = [...clusterMap.keys()]
+  ctx.fillStyle = rect.color
+  ctx.fillRect(rect.x, rect.y, rect.w, rect.h)
+}
 
-  const zScoresByTranscript = new Map(transcripts.map(transcript => {
-    const zScoresForTranscript = scDataset.getScaledCountsForTranscript(transcript)
+function drawText(
+  ctx: CanvasRenderingContext2D,
+  text: CanvasText
+) {
+  ctx.fillStyle = text.color
+  ctx.textBaseline = text.baseline
+  ctx.textAlign = text.align
+  ctx.font = text.font
+  ctx.fillText(text.text, text.x, text.y, text.maxWidth)
+}
 
-    const clusterMap: Map<string, number[]> = new Map(
-      clusters.map(cluster => [ cluster, [] ]))
 
-    zScoresForTranscript.forEach((zScore, cell) => {
-      if (!clusterMap.has(cell.clusterID)) return
-      clusterMap.get(cell.clusterID)!.push(zScore)
-    })
-
-    return [ transcript, clusterMap ]
-  }))
-
-  const ctx = canvasEl.getContext('2d')!
+function heatmapDimensions(
+  rect: { width: number, height: number },
+  transcripts: string[],
+  zScoresByCluster: ZScoreByCluster
+): HeatMapData {
+  const { height, width } = rect
 
   const colorScale = d3.scaleLinear<string>()
     .domain([ -1, 1, 2 ])
     .range([ '#ff00ff', 'black', 'yellow' ])
     .clamp(true)
 
-  const w = (rect.width - CLUSTERS_START_LEFT - PADDING_RIGHT) / clusters.length
-      , h = CLUSTER_GRID_HEIGHT / transcripts.length
+  const gridX = PADDING_LEFT + TRANSCRIPT_LABEL_WIDTH + TEXT_GAP
+      , gridY = PADDING_TOP + CLUSTER_LEGEND_HEIGHT
 
-  // ctx.scale(.1, .1)
-  let clustersDrawn = false
+  const grid = {
+    x: gridX,
+    y: gridY,
+    w: (width - gridX - PADDING_RIGHT) / zScoresByCluster.size,
+    h: (height - gridY - SCALE_HEIGHT) / (transcripts.length + 1),
+  }
 
-  Array.from(zScoresByTranscript).forEach(([ transcript, zScoresByCluster ], transcriptIdx) => {
-    if (transcriptIdx > 0) {
-      clustersDrawn = true
+  const clusters: HeatMapCluster[] = [...zScoresByCluster.values()].map((cluster, clusterIdx) => {
+    const clusterStartX = grid.x + clusterIdx * grid.w
+
+    const squares = Array.from(cluster.transcripts).map(([ transcript, zScores ], transcriptIdx) => ({
+      transcript,
+      x: clusterStartX + GRID_GAP,
+      y: grid.y + transcriptIdx * grid.h,
+      w: grid.w - GRID_GAP,
+      h: grid.h,
+      color: colorScale(d3.mean(zScores)!),
+    }))
+
+    const bar = {
+      x: clusterStartX + GRID_GAP,
+      y: grid.y - CLUSTER_LEGEND_GAP - CLUSTER_LEGEND_BAR_HEIGHT,
+      w: grid.w - GRID_GAP,
+      h: CLUSTER_LEGEND_BAR_HEIGHT,
+      color: cluster.cluster.color,
     }
 
-    // Draw the transcript labels
-    ctx.fillStyle = 'black'
-    ctx.textAlign = 'end'
-    ctx.textBaseline = 'middle'
-    ctx.font = '24px sans-serif'
-    ctx.fillText(
-      transcript,
-      CLUSTERS_START_LEFT - TEXT_GAP + GRID_GAP,
-      CLUSTERS_START_TOP + transcriptIdx * h + h / 2,
-      TRANSCRIPT_LABEL_WIDTH)
+    const label = {
+      text: cluster.cluster.label,
+      color: 'black',
+      font: '24px sans-serif',
+      align: 'center' as CanvasTextAlign,
+      baseline: 'alphabetic' as CanvasTextBaseline,
+      x: clusterStartX + grid.w / 2,
+      y: bar.y - TEXT_GAP,
+    }
 
-    Array.from(zScoresByCluster).forEach(([ clusterID, zScoresByCell ], clusterIdx) => {
-      const cluster = clusterMap.get(clusterID)!
-          , clusterStart = CLUSTERS_START_LEFT + w * clusterIdx
+    let maxY = bar.y + bar.h
 
-      if (!clustersDrawn) {
-        // Draw the cluster legend
-        const legendBarStartY = CLUSTERS_START_TOP - CLUSTER_LEGEND_GAP - CLUSTER_LEGEND_BAR_HEIGHT
+    const lastSquare = squares.slice(-1)[0]
 
-        // Draw the cluster bars
-        ctx.fillStyle = cluster.color
-        ctx.fillRect(
-          clusterStart + GRID_GAP,
-          legendBarStartY,
-          w - GRID_GAP,
-          CLUSTER_LEGEND_BAR_HEIGHT)
+    if (lastSquare) {
+      maxY = lastSquare.y + lastSquare.h
+    }
 
-        // Draw the cluster label
-        ctx.fillStyle = 'black'
-        ctx.textBaseline = 'alphabetic'
-        ctx.textAlign = 'center'
-        ctx.font = '24px sans-serif'
-        ctx.fillText(
-          cluster.label,
-          clusterStart + w / 2,
-          legendBarStartY - TEXT_GAP,
-          w)
-      }
+    return {
+      x: bar.x,
+      y: bar.y,
+      w: bar.w,
+      h: maxY - bar.y,
+      squares,
+      legend: {
+        bar,
+        label,
+      },
+    }
+  })
 
-      // Draw the cluster z score for this transcript
-      const meanZScore = d3.mean(zScoresByCell)!
-      ctx.fillStyle = colorScale(meanZScore)
-      ctx.fillRect(
-        clusterStart + GRID_GAP,
-        CLUSTERS_START_TOP + transcriptIdx * h,
-        w - GRID_GAP,
-        h)
+  const tickScale = d3.scaleLinear()
+    .range([grid.x, grid.x + SCALE_WIDTH])
+    .domain(d3.extent(colorScale.domain()) as [ number, number ])
+
+  const scaleTop = height - SCALE_HEIGHT
+      , ticks = colorScale.ticks(SCALE_TICKS)
+      , tickWidth = Math.ceil(tickScale(ticks[1]!) - tickScale(ticks[0]!))
+      , scaleLabels = [ 0, 1, 2 ]
+
+  const scale = {
+    marks: ticks.map(num => ({
+      color: colorScale(num),
+      x: tickScale(num),
+      y: scaleTop,
+      w: tickWidth,
+      h: SCALE_LEGEND_HEIGHT,
+    })),
+    labels: scaleLabels.map(num => ({
+      text: num.toString(),
+      font: '24px sans-serif',
+      color: 'black',
+      baseline: 'top' as CanvasTextBaseline,
+      align: 'center' as CanvasTextAlign,
+      x: tickScale(num),
+      y: scaleTop + SCALE_LEGEND_HEIGHT + TEXT_GAP,
+    })),
+  }
+
+  const transcriptLabels = transcripts.map((transcript, idx) => ({
+    text: transcript,
+    color: 'black',
+    font: '24px sans-serif',
+    align: 'right' as CanvasTextAlign,
+    baseline: 'middle' as CanvasTextBaseline,
+    x: grid.x - TEXT_GAP + GRID_GAP,
+    y: grid.y + idx * grid.h + grid.h / 2,
+  }))
+
+  return {
+    scale,
+    clusters,
+    transcriptLabels,
+  }
+}
+
+function drawHeatmapWithBlocks(
+  canvasEl: HTMLCanvasElement,
+  clusters: SeuratCluster[],
+  scDataset: ReturnType<typeof useSeuratDataset>,
+  rect: { width: number, height: number }
+) {
+  const zScores: ZScoreByCluster = new Map()
+
+  clusters.forEach(cluster => {
+    zScores.set(cluster.id, {
+      cluster,
+      transcripts: new Map(transcripts.map(transcript => [ transcript, [] ])),
     })
   })
 
-  const legendTicks = [0, 1, 2]
-      , tickScale = d3.scaleLinear().range([CLUSTERS_START_LEFT, CLUSTERS_START_LEFT + LEGEND_WIDTH]).domain([-1, 2])
-      , ticks = colorScale.ticks(LEGEND_TICKS)
-      , tickWidth = Math.ceil(tickScale(ticks[1]!) - tickScale(ticks[0]!))
+  transcripts.forEach(transcript => {
+    const zScoresForTranscript = scDataset.getScaledCountsForTranscript(transcript)
 
-  const legendTop = CLUSTERS_START_TOP + transcripts.length * h + LEGEND_PADDING_TOP
-
-  // Draw the colors on the color scale
-  colorScale.ticks(LEGEND_TICKS).forEach(num => {
-    ctx.fillStyle = colorScale(num)
-    ctx.fillRect(
-      tickScale(num),
-      legendTop,
-      tickWidth,
-      h)
+    zScoresForTranscript.forEach((zScore, cell) => {
+      if (!zScores.has(cell.clusterID)) return
+      zScores.get(cell.clusterID)!.transcripts.get(transcript)!.push(zScore)
+    })
   })
 
-  // Draw a few ticks on the color scale
-  legendTicks.forEach(val => {
-    ctx.fillStyle = 'black'
-    ctx.font = '24px sans-serif'
-    ctx.textBaseline = 'top'
-    ctx.fillText(
-      val.toString(),
-      tickScale(val),
-      legendTop + h + TEXT_GAP)
+  const heatmap = heatmapDimensions(rect, transcripts, zScores)
+
+  const ctx = canvasEl.getContext('2d')!
+
+  heatmap.clusters.forEach(cluster => {
+    cluster.squares.forEach(square => {
+      drawRect(ctx, square)
+    })
+
+    drawRect(ctx, cluster.legend.bar)
+    drawText(ctx, cluster.legend.label)
   })
+
+  heatmap.transcriptLabels.forEach(label => {
+    drawText(ctx, label)
+  })
+
+  heatmap.scale.marks.forEach(rect => {
+    drawRect(ctx, rect)
+  })
+
+  heatmap.scale.labels.forEach(text => {
+    drawText(ctx, text)
+  })
+
 }
 
 export default function HeatMap() {
@@ -158,7 +276,7 @@ export default function HeatMap() {
 
     if (canvasEl === null || rect === null) return
 
-    drawHeatmapWithBlocks(canvasEl, clusters, scDataset, rect)
+    drawHeatmapWithBlocks(canvasEl, Array.from(clusters.values()), scDataset, rect)
   }, [ canvasRef.current ])
 
   return (
