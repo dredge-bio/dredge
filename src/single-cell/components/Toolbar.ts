30a881abdd8b4edc1e8f864e4a4caa04331cfc25
Patrick Golden <ptgolden0@gmail.com>
2021-11-18 17:43:08 -0500

    Add an export dialog to the single cell table toolbar

--- src/single-cell/components/Toolbar.ts
+++ src/single-cell/components/Toolbar.ts
@@ -1,14 +1,181 @@
-import { createElement as h, useState } from 'react'
+import { createElement as h, useState, useEffect } from 'react'
 import { Flex, Button, Text } from 'rebass'
+import styled from 'styled-components'
 import { useView, useViewDispatch, useViewOptions } from '../hooks'
 import * as viewActions from '../actions'
 import { SearchTranscripts } from '@dredge/shared'
+import { Box } from 'rebass'
+
+const ExportDialogContainer = styled(Box)`
+  position: absolute;
+  background-color: white;
+  border: 1px solid #ccc;
+  top: calc(100% - 10px);
+  border-radius: 4px;
+  box-shadow: 2px 2px 4px #999;
+  left: 16px;
+  right: 16px;
+  z-index: 10;
+
+  label {
+    margin-right: 48px;
+  }
+
+  .export-grid {
+    display: grid;
+    grid-template-columns: auto auto auto auto 1fr;
+    align-items: center;
+  }
+
+  h3 {
+    margin-top: 12px;
+    margin-bottom: 4px;
+    grid-column: 1 / span 5;
+  }
+
+  input[disabled] + label {
+    color: #666;
+    cursor: not-allowed;
+  }
+
+  input[disabled] {
+    cursor: not-allowed;
+  }
+`
+
+type ExportDialogProps = {
+  hideDialog: () => void;
+}
+
+function ExportDialog(props: ExportDialogProps) {
+  const { hideDialog } = props
+      , id = Math.random().toString()
+      , view = useView()
+      , { selectedTranscripts, selectedClusters } = view
+
+    /*
+     * FIXME: why does the view change on every render?
+  useEffect(() => {
+    hideDialog()
+  }, [ view ])
+    */
+
+  const [ transcriptOption, setTranscriptOption ] = useState(
+    (selectedTranscripts.size ? 'selected' : 'all') as 'selected' | 'all')
+
+  const [ clusterOption, setClusterOption ] = useState(
+    (selectedClusters.size ? 'selected' : 'all') as 'selected' | 'all')
+
+  return (
+    h(ExportDialogContainer, {
+      p: 3,
+    }, ...[
+      h('h2', null, 'Export'),
+
+      h(Box, {
+        className: 'export-grid',
+      }, ...[
+        h('h3', null, 'Transcripts'),
+
+        h('input', {
+          type: 'radio',
+          id: `${id}-selected-transcripts`,
+          name: `${id}-transcripts`,
+          disabled: selectedTranscripts.size === 0,
+          checked: transcriptOption === 'selected',
+          onChange() {
+            setTranscriptOption('selected')
+          },
+        }),
+
+        h('label', {
+          type: 'radio',
+          htmlFor: `${id}-selected-transcripts`,
+        }, 'Selected transcripts'),
+
+        h('input', {
+          type: 'radio',
+          id: `${id}-all-transcripts`,
+          name: `${id}-transcripts`,
+          checked: transcriptOption === 'all',
+          onChange() {
+            setTranscriptOption('all')
+          },
+        }),
+
+        h('label', {
+          type: 'radio',
+          htmlFor: `${id}-all-transcripts`,
+        }, 'All transcripts'),
+
+        h('div'),
+
+        h('h3', null, 'Clusters'),
+
+        h('input', {
+          type: 'radio',
+          id: `${id}-selected-clusters`,
+          name: `${id}-clusters`,
+          disabled: selectedClusters.size === 0,
+          checked: clusterOption === 'selected',
+          onChange() {
+            setClusterOption('selected')
+          },
+        }),
+
+        h('label', {
+          type: 'radio',
+          htmlFor: `${id}-selected-clusters`,
+        }, 'Selected clusters'),
+
+        h('input', {
+          type: 'radio',
+          id: `${id}-all-clusters`,
+          name: `${id}-clusters`,
+          checked: clusterOption === 'all',
+          onChange() {
+            setClusterOption('all')
+          },
+        }),
+
+        h('label', {
+          type: 'radio',
+          htmlFor: `${id}-all-clusters`,
+        }, 'All clusters'),
+      ]),
+
+      h(Box, {
+        mt: 3,
+        className: 'export-buttons',
+      }, ...[
+        h(Button, {
+          mr: 2,
+          style: {
+            backgroundColor: 'blue',
+            color: 'white',
+          },
+        }, 'Export'),
+
+        h(Button, {
+          onClick() {
+            hideDialog()
+          },
+          style: {
+            backgroundColor: 'red',
+            color: 'white',
+          },
+        }, 'Cancel'),
+      ]),
+    ])
+  )
+}
 
 export default function Toolbar() {
   const { selectedTranscripts, project } = useView()
       , dispatch = useViewDispatch()
       , [ , updateOptions ] = useViewOptions()
       , [ showSearch, setShowSearch ] = useState(false)
+      , [ showExport, setShowExport ] = useState(false)
 
   return (
     h(Flex, {
@@ -32,6 +199,12 @@ export default function Toolbar() {
             updateOptions({
               selectedTranscripts: newSelectedTranscripts,
             })
+          },
+        }),
+
+        !showExport ? null : h(ExportDialog, {
+          hideDialog: () => {
+            setShowExport(false)
           },
         }),
 
@@ -73,6 +246,9 @@ export default function Toolbar() {
       h('div', null, ...[
         h(Button, {
           mx: 2,
+          onClick() {
+            setShowExport(prev => !prev)
+          },
         }, 'Export'),
 
         h(Button, {
