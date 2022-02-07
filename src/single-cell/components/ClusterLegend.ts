5a592dca23573ef514f864245196cd89356af503
Patrick Golden <ptgolden0@gmail.com>
2022-02-07 14:25:11 -0500

    Sync hovering clusters in legend with the rest of the clusters

--- src/single-cell/components/ClusterLegend.ts
+++ src/single-cell/components/ClusterLegend.ts
@@ -1,54 +1,88 @@
-import { createElement as h } from 'react'
+import { createElement as h, useState } from 'react'
 import { Box } from 'rebass'
 import styled from 'styled-components'
-import { useView } from '../hooks'
+import { useView, useViewDispatch } from '../hooks'
+import * as actions from '../actions'
 
 const Container = styled(Box)`
   height: 100%;
   max-height: 100%;
   overflow: auto;
+
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+
+  .legend-cluster {
+      display: inline-flex;
+      align-items: center;
+      padding: 4px;
+      column-gap: 6px;
+
+      border: 1px solid transparent;
+      cursor: pointer;
+  }
+
+  .legend-cluster[data-cluster-hovered="true"] {
+    border-color: black;
+    background-color: #f0f0f0;
+  }
+
+  .legend-cluster span {
+      display: inline-block;
+  }
 `
 
 export default function ClusterLegend() {
-  const { project } = useView()
+  const { project, hoveredCluster } = useView()
+      , dispatch = useViewDispatch()
       , { clusters } = project.data
+      , [ localHoveredCluster, setLocalHoveredCluster ] = useState<string | null>(null)
 
   return (
     h(Container, {
       bg: 'white',
-      py: 2,
-      px: 3,
-      style: {
-        display: 'flex',
-        flexDirection: 'column',
-        flexWrap: 'wrap',
-      },
+      p: 2,
     }, [...clusters.values()].map(cluster => (
       h('div', {
         key: cluster.id,
-        style: {
-          display: 'inline-flex',
-          alignItems: 'center',
-          margin: '4px 8px',
+        className: 'legend-cluster',
+        ['data-cluster-hovered']: (
+          localHoveredCluster === cluster.id ||
+            hoveredCluster.cluster?.id === cluster.id
+        ),
+        onMouseEnter() {
+          setLocalHoveredCluster(cluster.id)
+          dispatch(actions.setHoveredCluster({
+            cluster,
+            source: 'HeatMap',
+          }))
+        },
+        onMouseLeave() {
+          setLocalHoveredCluster(null)
+          dispatch(actions.setHoveredCluster({
+            cluster: null,
+            source: 'HeatMap',
+          }))
         },
       }, ...[
         h('span', {
           style: {
-            display: 'inline-block',
+            marginRight: '6px',
             width: '30px',
             height: '30px',
-            marginRight: '4px',
             background: cluster.color,
           },
         }),
 
         h('span', {
           style: {
-            display: 'inline-block',
+            fontWeight: 'bold',
+            marginRight: '5px',
           },
-        }, ...[
-          cluster.id,
-          ' = ',
+        }, cluster.id + '. '),
+
+        h('span', null, ...[
           cluster.label,
         ]),
       ])
